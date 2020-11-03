79c37c287d315c58c3909f6d33847a15152b8e79
ben <beatuscanis@hotmail.com>
2020-11-03 08:38:57 +0100

    done, but not sure, since the instructions were a littleunclear

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    status: false
+  }
+
+  changeColor = (e) => {
+    document.body.style.backgroundColor = e.target.value
+    this.setState({
+      status: true
+    })
+    console.dir(e.target.value)
+  }
+
+  changeback = (e) => {
+    document.body.style.backgroundColor = e.target.value
+    this.setState({
+      status: false
+    })
+    console.dir(e.target.name)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Components 1</h1>
+        <div style={{ display: "flex" }}>
+          {this.state.status ? <h3 className="apple fruits"> Apple, color = green</h3> : null}
+          <button onClick={this.changeColor} type="submit" name="changeColor" value="green" >Change Color</button>
+          <button onClick={this.changeback} type="submit" name="delete" value="white" >Delete Me</button>
+        </div>
+        <div style={{ display: "flex" }}>
+          {this.state.status ? <h3 className="apple fruits" >Mango, color = orange</h3> : null}
+
+          <button onClick={this.changeColor} type="submit" name="changeColor" value="orange" >Change Color</button>
+          <button onClick={this.changeback} type="submit" name="delete" value="white" >Delete Me</button>
+        </div>
+        <div style={{ display: "flex" }}>
+          {this.state.status ? <h3 className="apple fruits" >Banana, color = gold</h3> : null}
+
+          <button onClick={this.changeColor} type="submit" name="changeColor" value="gold" >Change Color</button>
+          <button onClick={this.changeback} type="submit" name="delete" value="white" >Delete Me</button>
+        </div>
+        <div style={{ display: "flex" }}>
+          {this.state.status ? <h3 className="apple fruits" >Lemon, color = yellow</h3> : null}
+
+          <button onClick={this.changeColor} type="submit" name="changeColor" value="yellow" >Change Color</button>
+          <button onClick={this.changeback} type="submit" name="delete" value="white" >Delete Me</button>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 
-export default App;
+
